2afbcb9fd512d65288783c9322c012478466fd6f
Simon Compston <compston@gmail.com>
2018-12-18 11:15:35 +0000

    allow secure/insecure connections

--- cmd/retag/main.go
+++ cmd/retag/main.go
@@ -14,39 +14,45 @@ import (
 )
 
 func splitTag(tag string) (string, string, string, error) {
-	split1 := strings.Split(tag, "/")
-	if len(split1) != 2 {
+	split1 := strings.SplitN(tag, "/", 2)
+	if len(split1) < 2 {
 		return "", "", "", errors.New("invalid tag " + tag)
 	}
-	split2 := strings.Split(split1[1], ":")
+	split2 := strings.SplitN(split1[1], ":", 2)
 	if len(split2) != 2 {
 		return "", "", "", errors.New("invalid tag " + tag)
 	}
 	return split1[0], split2[0], split2[1], nil
 }
 
-func registryURL(registry string, repo string, tag string) string {
-	return fmt.Sprintf("http://%s/v2/%s/manifests/%s", registry, repo, tag)
+func registryURL(insecure bool, registry string, repo string, tag string) string {
+	scheme := "https"
+	if insecure {
+		scheme = "http"
+	}
+	return fmt.Sprintf("%s://%s/v2/%s/manifests/%s", scheme, registry, repo, tag)
 }
 
 func main() {
 	var fromTag string
 	var toTag string
 	var dockerConfigFile string
+	var insecure bool
 	flag.StringVar(&fromTag, "from-tag", "", "the existing image tag")
 	flag.StringVar(&toTag, "to-tag", "", "the new image tag - must have the same registry and repo")
 	flag.StringVar(&dockerConfigFile, "docker-config", "", "the path to the Docker registry config.json file, used to obtain login credentials")
+	flag.BoolVar(&insecure, "insecure", false, "whether to use a http connection to the registry")
 	flag.Parse()
 	if fromTag == "" || toTag == "" {
 		log.Fatal("Error: must supply both from-tag and to-tag")
 	}
 	registry1, repo1, tag1, err := splitTag(fromTag)
 	if err != nil {
-		log.Fatal("failed to parse from-tag", err)
+		log.Fatal("failed to parse from-tag ", err)
 	}
 	registry2, repo2, tag2, err := splitTag(toTag)
 	if err != nil {
-		log.Fatal("failed to parse to-tag", err)
+		log.Fatal("failed to parse to-tag ", err)
 	}
 	if registry1 != registry2 || repo1 != repo2 {
 		log.Fatalf("registry and repo must be the same: %s != %s, or %s != %s", registry1, registry2, repo1, repo2)
@@ -65,12 +71,13 @@ func main() {
 
 	log.Printf("from %s (%s, %s, %s)", fromTag, registry1, repo1, tag1)
 	log.Printf("to %s (%s, %s, %s)", toTag, registry2, repo2, tag2)
-	log.Println("using " + dockerConfigFile)
+	log.Println("using ", dockerConfigFile)
+	log.Println("insecure connection? ", insecure)
 
-	url1 := registryURL(registry1, repo1, tag1)
-	url2 := registryURL(registry2, repo2, tag2)
-	log.Println("with from url", url1)
-	log.Println("with to url", url2)
+	url1 := registryURL(insecure, registry1, repo1, tag1)
+	url2 := registryURL(insecure, registry2, repo2, tag2)
+	log.Println("with from url ", url1)
+	log.Println("with to url ", url2)
 
 	client := client.CreateClient(dockerConfig)
 	manifest, err := client.GetV2Manifest(url1)
